1026f60fc8c8afc0c6a788e31ffddfe7895087ae
Travis Grimmond <travisslack1994@outlook.com>
2017-10-09 23:37:51 +0100

    adds changes, back to start of file

--- front-end-gametime/src/App.js
+++ front-end-gametime/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
 
+
 class App extends Component {
 
   constructor() {
@@ -36,7 +37,7 @@ class App extends Component {
           <h1 className="App-title">Games Consoles</h1>
         </header>
         <section>
-        
+          
         </section>
       </div>
     );
--- game_time_app/app/controllers/consoles_controller.rb
+++ game_time_app/app/controllers/consoles_controller.rb
@@ -1,12 +1,16 @@
 class ConsolesController < ApplicationController
 
+    def console_params
+        params.require(:show).permit([:title, :released, :description, :image])
+    end
+    
     def index
         console = Console.all 
         render :json => console
     end
 
     def show
-        console = Console.find(params[:id])
+        console = Console.find(console_params)
         render :json => console
     end
 
